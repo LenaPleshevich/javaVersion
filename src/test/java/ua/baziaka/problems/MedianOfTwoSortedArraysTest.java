ae275f50eeedf92082f6cba8bd5fd9a4972bf563
Dmytro Baziaka <dmytro_baziaka@epam.com>
2019-07-19 16:55:51 +0300

    Removed task solution.

--- src/test/java/ua/baziaka/problems/MedianOfTwoSortedArraysTest.java
+++ src/test/java/ua/baziaka/problems/MedianOfTwoSortedArraysTest.java
@@ -1,12 +1,7 @@
 package ua.baziaka.problems;
 
-import lombok.AllArgsConstructor;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.Arrays;
-import java.util.List;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -19,25 +14,9 @@ public class MedianOfTwoSortedArraysTest {
         toTestAlg = new MedianOfTwoSortedArrays4();
     }
 
-    @ParameterizedTest
-    @MethodSource("testData")
-    public void testMedianOfTwoSortedArrays(MedianOfTwoSortedArraysTestData testData) {
-        double actual = toTestAlg.findMedianSortedArrays(testData.nums1, testData.nums2);
-        assertEquals(testData.output, actual);
+    @Test
+    public void testMedianOfTwoSortedArrays() {
+        double actual = toTestAlg.findMedianSortedArrays(new int[]{1, 3},  new int[]{2});
+        assertEquals(2.0d, actual);
     }
-
-    public static List<MedianOfTwoSortedArraysTestData> testData() {
-        return Arrays.asList(
-                new MedianOfTwoSortedArraysTestData(new int[]{1, 3}, new int[]{2}, 2.0d),
-                new MedianOfTwoSortedArraysTestData(new int[]{1, 2}, new int[]{3, 4}, 2.5d)
-        );
-    }
-
-    @AllArgsConstructor
-    static class MedianOfTwoSortedArraysTestData {
-        int[] nums1;
-        int[] nums2;
-        double output;
-    }
-
 }
