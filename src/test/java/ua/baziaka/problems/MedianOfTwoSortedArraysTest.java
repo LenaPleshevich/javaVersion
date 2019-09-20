c39c3c30b8a93d51d2ac785ce4cd8acc2eefc537
Alena_Pliashevich <alena_pliashevich@epam.com>
2019-09-20 13:01:47 +0300

    added stream to test

--- src/test/java/ua/baziaka/problems/MedianOfTwoSortedArraysTest.java
+++ src/test/java/ua/baziaka/problems/MedianOfTwoSortedArraysTest.java
@@ -3,6 +3,8 @@ package ua.baziaka.problems;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
+import java.util.stream.IntStream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MedianOfTwoSortedArraysTest {
@@ -11,6 +13,7 @@ public class MedianOfTwoSortedArraysTest {
 
     @Test
     public void testSuccess(){
+        IntStream.iterate(1, i -> i < 100, i -> i + 1).forEach(System.out::println);
         Assert.assertEquals(2,2);
     }
 }
