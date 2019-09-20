835819af1a9afa5e25c2a54dc530a4980aa6fe81
Alena_Pliashevich <alena_pliashevich@epam.com>
2019-09-20 11:43:26 +0300

    add stream API

--- src/main/java/ua/baziaka/problems/MedianOfTwoSortedArrays.java
+++ src/main/java/ua/baziaka/problems/MedianOfTwoSortedArrays.java
@@ -1,10 +1,13 @@
 package ua.baziaka.problems;
 
+import java.util.stream.IntStream;
+
 /**
  * https://leetcode.com/problems/median-of-two-sorted-arrays/description/
  */
 public class MedianOfTwoSortedArrays {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        IntStream.iterate(1, i -> i < 100, i -> i + 1).forEach(System.out::println);
         throw new RuntimeException("Not implemented yet");
     }
 }
