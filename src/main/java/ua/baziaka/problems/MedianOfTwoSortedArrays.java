2fe24ff4b2efae15a0965173190e47e0527f2345
Alena_Pliashevich <alena_pliashevich@epam.com>
2019-09-27 14:18:47 +0300

    added features java10

--- src/main/java/ua/baziaka/problems/MedianOfTwoSortedArrays.java
+++ src/main/java/ua/baziaka/problems/MedianOfTwoSortedArrays.java
@@ -1,13 +1,15 @@
 package ua.baziaka.problems;
 
-import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * https://leetcode.com/problems/median-of-two-sorted-arrays/description/
  */
 public class MedianOfTwoSortedArrays {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        IntStream.iterate(1, i -> i < 100, i -> i + 1).forEach(System.out::println);
-        return 2;
+    public void findMedianSortedArrays() {
+        var firstEven = Stream.of(1,2,3,4).filter(i -> i % 2 == 0)
+                .findFirst()
+                .orElseThrow();
+        System.out.println(firstEven);
     }
 }
--- src/test/java/ua/baziaka/problems/MedianOfTwoSortedArraysTest.java
+++ src/test/java/ua/baziaka/problems/MedianOfTwoSortedArraysTest.java
@@ -3,9 +3,7 @@ package ua.baziaka.problems;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
-import java.util.stream.IntStream;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.math.BigDecimal;
 
 public class MedianOfTwoSortedArraysTest {
 
@@ -13,7 +11,10 @@ public class MedianOfTwoSortedArraysTest {
 
     @Test
     public void testSuccess(){
-        IntStream.iterate(1, i -> i < 100, i -> i + 1).forEach(System.out::println);
+        //IntStream.iterate(1, i -> i < 100, i -> i + 1).forEach(System.out::println);
+        BigDecimal price = new BigDecimal("1000000");
+        System.out.println(price);
         Assert.assertEquals(2,2);
     }
+
 }
